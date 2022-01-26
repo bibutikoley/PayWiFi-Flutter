16edfd051edb9cb022f130ef1a410a88044bfdc6
Bibuti Koley <bibutikoley@outlook.com>
2022-01-27 00:53:43 +0530

    Soft keyboard is hidden on focus change for a view

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -27,166 +27,174 @@ class LoginPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      body: SafeArea(
-        child: SingleChildScrollView(
-          child: Padding(
-            padding: const EdgeInsets.fromLTRB(24.0, 40.0, 24.0, 0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      'Login to your\naccount',
-                      style: heading2.copyWith(color: textBlack),
-                    ),
-                    const SizedBox(
-                      height: 20,
-                    ),
-                    Image.asset(
-                      'assets/images/accent.png',
-                      width: 99,
-                      height: 4,
-                    ),
-                  ],
-                ),
-                const SizedBox(
-                  height: 48,
-                ),
-                Form(
-                  child: Column(
+      body: GestureDetector(
+        onTap: () {
+          FocusScopeNode currentFocus = FocusScope.of(context);
+          if (!currentFocus.hasPrimaryFocus) {
+            currentFocus.unfocus();
+          }
+        },
+        child: SafeArea(
+          child: SingleChildScrollView(
+            child: Padding(
+              padding: const EdgeInsets.fromLTRB(24.0, 40.0, 24.0, 0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
-                      Container(
-                        decoration: BoxDecoration(
-                          color: textWhiteGrey,
-                          borderRadius: BorderRadius.circular(14.0),
-                        ),
-                        child: TextFormField(
-                          decoration: InputDecoration(
-                            hintText: 'Email',
-                            hintStyle: heading6.copyWith(color: textGrey),
-                            border: const OutlineInputBorder(
-                              borderSide: BorderSide.none,
-                            ),
-                          ),
-                        ),
+                      Text(
+                        'Login to your\naccount',
+                        style: heading2.copyWith(color: textBlack),
                       ),
                       const SizedBox(
-                        height: 32,
+                        height: 20,
                       ),
-                      Container(
-                        decoration: BoxDecoration(
-                          color: textWhiteGrey,
-                          borderRadius: BorderRadius.circular(14.0),
-                        ),
-                        child: Obx(
-                          () => TextFormField(
-                            obscureText:
-                                _authPageStateManager.isPasswordVisible.isFalse,
+                      Image.asset(
+                        'assets/images/accent.png',
+                        width: 99,
+                        height: 4,
+                      ),
+                    ],
+                  ),
+                  const SizedBox(
+                    height: 48,
+                  ),
+                  Form(
+                    child: Column(
+                      children: [
+                        Container(
+                          decoration: BoxDecoration(
+                            color: textWhiteGrey,
+                            borderRadius: BorderRadius.circular(14.0),
+                          ),
+                          child: TextFormField(
                             decoration: InputDecoration(
-                              hintText: 'Password',
+                              hintText: 'Email',
                               hintStyle: heading6.copyWith(color: textGrey),
-                              suffixIcon: IconButton(
-                                color: textGrey,
-                                splashRadius: 1,
-                                icon: Icon(_authPageStateManager
-                                        .isPasswordVisible.isFalse
-                                    ? Icons.visibility_outlined
-                                    : Icons.visibility_off_outlined),
-                                onPressed: togglePassword,
-                              ),
                               border: const OutlineInputBorder(
                                 borderSide: BorderSide.none,
                               ),
                             ),
                           ),
                         ),
+                        const SizedBox(
+                          height: 32,
+                        ),
+                        Container(
+                          decoration: BoxDecoration(
+                            color: textWhiteGrey,
+                            borderRadius: BorderRadius.circular(14.0),
+                          ),
+                          child: Obx(
+                            () => TextFormField(
+                              obscureText: _authPageStateManager
+                                  .isPasswordVisible.isFalse,
+                              decoration: InputDecoration(
+                                hintText: 'Password',
+                                hintStyle: heading6.copyWith(color: textGrey),
+                                suffixIcon: IconButton(
+                                  color: textGrey,
+                                  splashRadius: 1,
+                                  icon: Icon(_authPageStateManager
+                                          .isPasswordVisible.isFalse
+                                      ? Icons.visibility_outlined
+                                      : Icons.visibility_off_outlined),
+                                  onPressed: togglePassword,
+                                ),
+                                border: const OutlineInputBorder(
+                                  borderSide: BorderSide.none,
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 32,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      AuthPageCheckBox(),
+                      const SizedBox(
+                        width: 12,
+                      ),
+                      GestureDetector(
+                          onTap: toggleRememberMeAction,
+                          child: Text('Remember me', style: regular16pt)),
+                    ],
+                  ),
+                  const SizedBox(
+                    height: 32,
+                  ),
+                  CustomPrimaryButton(
+                    buttonColor: primaryBlue,
+                    textValue: 'Login',
+                    textColor: Colors.white,
+                  ),
+                  const SizedBox(
+                    height: 24,
+                  ),
+                  Center(
+                    child: Text(
+                      'OR',
+                      style: heading6.copyWith(color: textGrey),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 24,
+                  ),
+                  CustomOutlinedButton(
+                    buttonStrokeColor: primaryBlue,
+                    textValue: 'Login with Google',
+                    textColor: textBlack,
+                  ),
+                  const SizedBox(
+                    height: 50,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        "Don't have an account? ",
+                        style: regular16pt.copyWith(color: textGrey),
+                      ),
+                      GestureDetector(
+                        onTap: () {
+                          Get.bottomSheet(
+                            SizedBox(
+                              height: (Get.height * 0.90),
+                              child: RegisterPage(),
+                            ),
+                            isScrollControlled: true,
+                            shape: const RoundedRectangleBorder(
+                              borderRadius: BorderRadius.vertical(
+                                top: Radius.circular(24),
+                              ),
+                            ),
+                            enterBottomSheetDuration:
+                                const Duration(milliseconds: 400),
+                            exitBottomSheetDuration:
+                                const Duration(milliseconds: 400),
+                            clipBehavior: Clip.antiAliasWithSaveLayer,
+                          );
+                        },
+                        child: Text(
+                          'Register',
+                          style: regular16pt.copyWith(color: primaryBlue),
+                        ),
                       ),
                     ],
                   ),
-                ),
-                const SizedBox(
-                  height: 32,
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  children: [
-                    AuthPageCheckBox(),
-                    const SizedBox(
-                      width: 12,
-                    ),
-                    GestureDetector(
-                        onTap: toggleRememberMeAction,
-                        child: Text('Remember me', style: regular16pt)),
-                  ],
-                ),
-                const SizedBox(
-                  height: 32,
-                ),
-                CustomPrimaryButton(
-                  buttonColor: primaryBlue,
-                  textValue: 'Login',
-                  textColor: Colors.white,
-                ),
-                const SizedBox(
-                  height: 24,
-                ),
-                Center(
-                  child: Text(
-                    'OR',
-                    style: heading6.copyWith(color: textGrey),
+                  const SizedBox(
+                    height: 50,
                   ),
-                ),
-                const SizedBox(
-                  height: 24,
-                ),
-                CustomOutlinedButton(
-                  buttonStrokeColor: primaryBlue,
-                  textValue: 'Login with Google',
-                  textColor: textBlack,
-                ),
-                const SizedBox(
-                  height: 50,
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text(
-                      "Don't have an account? ",
-                      style: regular16pt.copyWith(color: textGrey),
-                    ),
-                    GestureDetector(
-                      onTap: () {
-                        Get.bottomSheet(
-                          SizedBox(
-                            height: (Get.height * 0.90),
-                            child: RegisterPage(),
-                          ),
-                          isScrollControlled: true,
-                          shape: const RoundedRectangleBorder(
-                            borderRadius: BorderRadius.vertical(
-                              top: Radius.circular(24),
-                            ),
-                          ),
-                          enterBottomSheetDuration:
-                              const Duration(milliseconds: 400),
-                          exitBottomSheetDuration:
-                              const Duration(milliseconds: 400),
-                          clipBehavior: Clip.antiAliasWithSaveLayer,
-                        );
-                      },
-                      child: Text(
-                        'Register',
-                        style: regular16pt.copyWith(color: primaryBlue),
-                      ),
-                    ),
-                  ],
-                ),
-                const SizedBox(
-                  height: 50,
-                ),
-              ],
+                ],
+              ),
             ),
           ),
         ),
